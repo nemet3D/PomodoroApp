47e71a85f053610ec721041c529af77fee1edc9c
nemet3D <nemet3D@gmail.com>
2019-05-29 17:29:35 +0300

    Counter appears in window after button press

--- guiApp.py
+++ guiApp.py
@@ -1,15 +1,84 @@
-from PyQt5.QtWidgets import *
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit
+from PyQt5.QtGui import QIcon
 
-app = QApplication([])
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
 
-button = QPushButton('Click')
+DURATION_INT = 25
 
-def on_button_clicked():
-    alert = QMessageBox()
-    alert.setText('You clicked the button!')
-    alert.exec_()
+class App(QWidget):
 
-button.clicked.connect(on_button_clicked)
-button.show()
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 input dialogs - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        self.getInteger()
+        
+        self.show()
+        
+    def getInteger(self):
+        i, okPressed = QInputDialog.getInt(self, "Insert time","Minutes:", 25, 5, 60, 1)
+        if okPressed:
+            print(i)
 
-app.exec_()
+class MyMainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.time_left_int = DURATION_INT
+        self.widget_counter_int = 0
+
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
+        vbox = QtWidgets.QVBoxLayout()
+        central_widget.setLayout(vbox)
+
+        self.pages_qsw = QtWidgets.QStackedWidget()
+        vbox.addWidget(self.pages_qsw)
+        self.time_passed_qll = QtWidgets.QLabel()
+        vbox.addWidget(self.time_passed_qll)
+
+        self.widget_one = QtWidgets.QLabel("Your pomodoroApp is counting")
+        self.pages_qsw.addWidget(self.widget_one)
+
+        self.timer_start()
+        self.update_gui()
+
+    def timer_start(self):
+        self.time_left_int = DURATION_INT
+
+        self.my_qtimer = QtCore.QTimer(self)
+        self.my_qtimer.timeout.connect(self.timer_timeout)
+        self.my_qtimer.start(1000)
+
+        self.update_gui()
+
+    def timer_timeout(self):
+        self.time_left_int -= 1
+
+        if self.time_left_int == 0:
+            self.widget_counter_int = (self.widget_counter_int + 1) % 4
+            self.pages_qsw.setCurrentIndex(self.widget_counter_int)
+            self.time_left_int = DURATION_INT
+
+        self.update_gui()
+
+    def update_gui(self):
+        self.time_passed_qll.setText(str(self.time_left_int))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    main_window = MyMainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
